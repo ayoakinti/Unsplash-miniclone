bb90c8c26f3018bfad29f6966bee871e2cab0597
Ayomide Akintimehin <ayoakinti@gmail.com>
2021-04-09 16:23:44 +0100

    attribute unsplash

--- components/ModalPhotoFigure.vue
+++ components/ModalPhotoFigure.vue
@@ -1,7 +1,7 @@
 <template>
   <figure id="modal-photo">
     <div class="image-container">
-      <img :src="photo.urls.raw" alt="photo" width="100%">
+      <img :src="photo.urls.regular" alt="photo" width="100%">
     </div>
     <figcaption>
       <h4 class="m-0">
@@ -10,6 +10,18 @@
       <p class="m-0">
         {{ photo.user.location }}
       </p>
+      <div class="m-0">
+        Photo by
+        <a
+          target="_blank"
+          :href="'https://unsplash.com/@'+photo.user.username+'?utm_source=your_app_name&utm_medium=referral'"
+        >{{ photo.user.first_name }} {{ photo.user.last_name }}</a>
+        on
+        <a
+          target="_blank"
+          href="https://unsplash.com/?utm_source=your_app_name&utm_medium=referral"
+        >Unsplash</a>
+      </div>
     </figcaption>
   </figure>
 </template>
