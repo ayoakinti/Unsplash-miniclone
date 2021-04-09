71a962486859bacd341a04be8cb13b718ced227d
Ayomide Akintimehin <ayoakinti@gmail.com>
2021-04-09 15:18:15 +0100

    svg edits

--- components/Icons/IconSearch.vue
+++ components/Icons/IconSearch.vue
@@ -1,13 +1,14 @@
 <template>
   <svg
-    width="32"
-    height="32"
+    width="22"
+    height="22"
     class="_3cOFI _2Jh5K _2_G8g"
     version="1.1"
     viewBox="0 0 32 32"
     aria-hidden="false"
   >
     <path
+      fill="#333"
       d="M22 20c1.2-1.6 2-3.7 2-6 0-5.5-4.5-10-10-10S4 8.5 4 14s4.5 10 10 10c2.3 0 4.3-.7 6-2l6.1 6 1.9-2-6-6zm-8 1.3c-4 0-7.3-3.3-7.3-7.3S10 6.7 14 6.7s7.3 3.3 7.3 7.3-3.3 7.3-7.3 7.3z"
     />
   </svg>
